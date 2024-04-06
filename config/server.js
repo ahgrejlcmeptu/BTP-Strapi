d9d4ba025207ff15eaca0de916ac374b1cfc7224
Victor Tkachev <bgblllhuk@gmail.com>
2024-04-06 16:44:06 +0800

    06.04

--- config/server.js
+++ config/server.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  admin: {
+    url: '/dashboard',
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
@@ -8,3 +11,4 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
+
